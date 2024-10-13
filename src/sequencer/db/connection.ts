6013b659146601cb5c8be5d48aecb2c32e90a3d8
kadirchan <kadircanbozkurt1905@gmail.com>
2024-10-14 00:41:08 +0300

    more cleanup

--- src/sequencer/db/connection.ts
+++ src/sequencer/db/connection.ts
@@ -1,20 +1,6 @@
 import mongoose from "mongoose";
 import logger from "../logger.js";
 
-// export function connectToDatabase() {
-//     mongoose.connect(process.env.MONGO as string);
-//     const db = mongoose.connection;
-
-//     db.on("error", (err) => {
-//         logger.error("MongoDB connection error:", err);
-//         throw new Error("MongoDB connection error");
-//     });
-
-//     db.once("open", () => {
-//         logger.info("Connected successfully to MongoDB");
-//     });
-// }
-
 export async function connectToDatabase() {
     try {
         if (!process.env.MONGO) {
